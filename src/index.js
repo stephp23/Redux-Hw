f1dd9ef4f8a79cebc6276af019062e1761cb04f6
stephp23 <stephp23@gmail.com>
2021-03-10 22:08:39 -0500

    part8

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reducer from "./store/reducer";
 
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducer from './store/reducer';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
+import { watchAgeUp } from "./sagas/saga";
 
-const store = createStore(reducer);
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+sagaMiddleware.run(watchAgeUp);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,18 +1,19 @@
 const initialState = {
-  age:21
+  age: 20
 };
 
-const reducer = (state=initialState, action) => {
+const reducer = (state = initialState, action) => {
   const newState = { ...state };
-  
-  if (action.type === 'AGE_UP') {
-    newState.age++;
-  }
 
-  if (action.type === 'AGE_DOWN') {
-    newState.age--;
-  }
+  switch (action.type) {
+    case "AGE_UP_ASYNC":
+      newState.age += action.value;
+      break;
 
+    case "AGE_DOWN":
+      newState.age -= action.value;
+      break;
+  }
   return newState;
 };
 
